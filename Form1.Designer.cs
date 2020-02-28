7c1504efdc65bf3617bcd3cd54b8d340f6806ad3
Frank Fiumara <33582916+frankpop10@users.noreply.github.com>
2020-02-28 07:24:48 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace fortdrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lemonada", 10.75F);
+            this.label2.Location = new System.Drawing.Point(504, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Designed by: HeyMaliibu";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(35, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 5;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 20.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(12, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "You\'re going to: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 19.25F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Where we dropping?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(203)))), ((int)(((byte)(237)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(227, 182);
+            this.panel1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
+            this.button1.BackgroundImage = global::fortdrop.Properties.Resources._91bac9568618da1c93b2f29927d5e006c3a11ee4r1_900_900v2_hq;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
+            this.button1.Location = new System.Drawing.Point(12, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 188);
+            this.button1.TabIndex = 1;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::fortdrop.Properties.Resources.agency;
+            this.pictureBox1.Location = new System.Drawing.Point(254, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(485, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(751, 462);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Lemonada", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fortdrop
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(set_background);
+
+            Button b = button1;
+            b.Click += new EventHandler(ShowMessage);
+            Controls.Add(b);
+        }
+        private void set_background(Object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            //the rectangle, the same size as our Form
+            Rectangle gradient_rectangle = new Rectangle(0, 0, Width, Height);
+
+            //define gradient's properties
+            Brush b = new LinearGradientBrush(gradient_rectangle, Color.FromArgb(134, 22, 87), Color.FromArgb(255, 166, 158), 326f);
+
+            //apply gradient         
+            graphics.FillRectangle(b, gradient_rectangle);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ShowMessage(object sender, EventArgs e)
+        {
+            String[] places;
+            places = new string[] { "The Shark", "The Yacht", "The Agency","The Grotto","The Rig","Salty Springs","Sweaty Sands","Pleasent Park","Holly Hedges",
+                "Weeping Woods","Slurpy Swamp","Misty Meadows","Lazy Lake", "Retail Row", "Dirty Docks", "Frenzy Farm", "Steamy Stacks","Craggy Cliffs"};
+            Random rand = new Random();
+
+            int index = rand.Next(places.Length);
+            label4.Text = places[index];
+            if (index == 0) {
+
+                pictureBox1.Image = fortdrop.Properties.Resources.shark;
+
+            } else if (index == 1) {
+                pictureBox1.Image = fortdrop.Properties.Resources.yacht;
+            }
+            else if (index == 2)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.agency;
+            }
+            else if (index == 3)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.grotto;
+            }
+            else if (index == 4)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.rig;
+            }
+            else if (index == 5)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.salty;
+            }
+            else if (index == 6)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.sands;
+            }
+            else if (index == 7)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.pleasent;
+            }
+            else if (index == 8)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.holly;
+            }
+            else if (index == 9)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.Weeping;
+            }
+            else if (index == 10)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.slurp;
+            }
+            else if (index == 11)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.misty;
+            }
+            else if (index == 12)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.lazy;
+            }
+            else if (index == 13)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.retail;
+            }
+            else if (index == 14)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.dirty;
+            }
+            else if (index == 15)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.frenzy;
+            }
+            else if (index == 16)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.steamy;
+            }
+            else if (index == 17)
+            {
+                pictureBox1.Image = fortdrop.Properties.Resources.craggy;
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
+
+            label4.Refresh();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
